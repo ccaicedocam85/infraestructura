97ecccb6250a83e83037ff3753a981b03c73f90d
Cristian Caicedo <ccaicedocam@gmail.com>
2024-04-07 13:35:30 -0500

    fmt proyect

--- terraform/modules/securitygroup/variables.tf
+++ terraform/modules/securitygroup/variables.tf
@@ -10,12 +10,12 @@ variable "name_security_group" {
 variable "ingress_rules" {
   description = "ingress rules"
   type = list(object({
-    from_port   = number
-    to_port     = number
-    protocol    = string
-    cidr_blocks = list(string)
+    from_port       = number
+    to_port         = number
+    protocol        = string
+    cidr_blocks     = list(string)
     security_groups = list(string)
-    description = string
+    description     = string
   }))
 }
 
